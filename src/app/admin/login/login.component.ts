6fbffb729d13894c81e49b336cf31fd9e554198e
orunsolukafilatoyinade <orunsolukafilat@gmail.com>
2026-03-19 09:03:42 +0000

    feat: back to home button on login page

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router , RouterLink} from '@angular/router';
 import { ApiService } from '../../services/api.service';
 
 @Component({
